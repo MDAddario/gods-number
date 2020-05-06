23970f5cf9ee92413b3802bde5eadae41f62876b
MDAddario <michael.lindner.daddario@gmail.com>
2020-05-06 19:10:42 -0400

    Updates Cube class to have a set of function pointers for the rotations

--- src/Cube.java
+++ src/Cube.java
@@ -8,6 +8,47 @@ class Cube {
     byte[] orangeFace;
     byte[]   blueFace;
 
+    // Color definitions
+    static final byte WHITE  = 0;
+    static final byte RED    = 1;
+    static final byte GREEN  = 2;
+    static final byte YELLOW = 3;
+    static final byte ORANGE = 4;
+    static final byte BLUE   = 5;
+
+    // Setup initial faces
+    private void setupInitialFaces() {
+
+        // Allocate memory
+        this. whiteFace = new byte[8];
+        this.   redFace = new byte[8];
+        this. greenFace = new byte[8];
+        this.yellowFace = new byte[8];
+        this.orangeFace = new byte[8];
+        this.  blueFace = new byte[8];
+
+        // Paint the faces
+        for (int i = 0; i < 8; i++) {
+            this. whiteFace[i] = WHITE;
+            this.   redFace[i] = RED;
+            this. greenFace[i] = GREEN;
+            this.yellowFace[i] = YELLOW;
+            this.orangeFace[i] = ORANGE;
+            this.  blueFace[i] = BLUE;
+        }
+    }
+
+    // See if cube is in the solved state
+    boolean isSolved() {
+
+        // Paint the faces
+        for (int i = 0; i < 8; i++)
+            if (    this. whiteFace[i] != WHITE  || this.   redFace[i] != RED    || this.greenFace[i] != GREEN ||
+                    this.yellowFace[i] != YELLOW || this.orangeFace[i] != ORANGE || this. blueFace[i] != BLUE)
+                return false;
+        return true;
+    }
+
     // Types of rotations (number of -90 degree rotations)
     static final byte ID   = 0;  // Identity
     static final byte CW   = 1;  // Clockwise
@@ -76,80 +117,90 @@ class Cube {
         fourCycle(topFace, 2, rightFace, 0, botFace, 7, leftFace, 5, rotationType);
     }
 
-    // Well defined rotation notation from speed-cubing community
-    void U (byte rotationType) {
-        rotateFace(whiteFace, redFace, greenFace, orangeFace, blueFace, rotationType);
+    // Create a rotation class for each of the faces
+    private abstract class Rotation {
+        abstract void rotate(byte rotationType);
     }
-    void R (byte rotationType) {
-        rotateFace(redFace, greenFace, whiteFace, blueFace, yellowFace, rotationType);
+    private class WhiteRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(whiteFace, redFace, greenFace, orangeFace, blueFace, rotationType);
+        }
     }
-    void F (byte rotationType) {
-        rotateFace(greenFace, whiteFace, redFace, yellowFace, orangeFace, rotationType);
+    private class RedRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(redFace, greenFace, whiteFace, blueFace, yellowFace, rotationType);
+        }
     }
-    void D (byte rotationType) {
-        rotateFace(yellowFace, blueFace, orangeFace, greenFace, redFace, rotationType);
+    private class GreenRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(greenFace, whiteFace, redFace, yellowFace, orangeFace, rotationType);
+        }
     }
-    void L (byte rotationType) {
-        rotateFace(orangeFace, yellowFace, blueFace, whiteFace, greenFace, rotationType);
+    private class YellowRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(yellowFace, blueFace, orangeFace, greenFace, redFace, rotationType);
+        }
     }
-    void B (byte rotationType) {
-        rotateFace(blueFace, orangeFace, yellowFace, redFace, whiteFace, rotationType);
+    private class OrangeRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(orangeFace, yellowFace, blueFace, whiteFace, greenFace, rotationType);
+        }
     }
+    private class BlueRotation extends Rotation{
+        void rotate(byte rotationType) {
+            rotateFace(blueFace, orangeFace, yellowFace, redFace, whiteFace, rotationType);
+        }
+    }
+
+    // Array of rotation instances
+    private Rotation[] rotations;
+
+    // Pair up each rotation with the associated face number
+    private void setupRotations() {
+
+        // Allocate memory
+        this.rotations = new Rotation[6];
+
+        // Place the rotations
+        this.rotations[WHITE]  = new  WhiteRotation();
+        this.rotations[RED]    = new    RedRotation();
+        this.rotations[GREEN]  = new  GreenRotation();
+        this.rotations[YELLOW] = new YellowRotation();
+        this.rotations[ORANGE] = new OrangeRotation();
+        this.rotations[BLUE]   = new   BlueRotation();
+    }
+
+    // Perform any possible rotation
+    void rotate(byte faceIndex, byte rotationType) {
+
+        // Ensure face index is valid
+        if (!(0 <= faceIndex && faceIndex < 6))
+            throw new IllegalArgumentException("Face index argument is illegal.");
+
+        // Perform the rotation
+        this.rotations[faceIndex].rotate(rotationType);
+    }
+
+    // Well defined rotation names in the speed-cubing community
+    void U(byte rotationType) { this.rotate(WHITE,  rotationType); }
+    void R(byte rotationType) { this.rotate(RED,    rotationType); }
+    void F(byte rotationType) { this.rotate(GREEN,  rotationType); }
+    void D(byte rotationType) { this.rotate(YELLOW, rotationType); }
+    void L(byte rotationType) { this.rotate(ORANGE, rotationType); }
+    void B(byte rotationType) { this.rotate(BLUE,   rotationType); }
 
     // Default constructor
     Cube() {
 
         // Setup default face layout
         this.setupInitialFaces();
-    }
 
-    // Color definitions
-    static final byte WHITE  = 0;
-    static final byte RED    = 1;
-    static final byte GREEN  = 2;
-    static final byte YELLOW = 3;
-    static final byte ORANGE = 4;
-    static final byte BLUE   = 5;
-
-    // Setup initial faces
-    private void setupInitialFaces() {
-
-        // Allocate memory
-        this. whiteFace = new byte[8];
-        this.   redFace = new byte[8];
-        this. greenFace = new byte[8];
-        this.yellowFace = new byte[8];
-        this.orangeFace = new byte[8];
-        this.  blueFace = new byte[8];
-
-        // Paint the faces
-        for (int i = 0; i < 8; i++) {
-            this. whiteFace[i] = WHITE;
-            this.   redFace[i] = RED;
-            this. greenFace[i] = GREEN;
-            this.yellowFace[i] = YELLOW;
-            this.orangeFace[i] = ORANGE;
-            this.  blueFace[i] = BLUE;
-        }
-    }
-
-    // See if cube is in the solved state
-    boolean isSolved() {
-
-        // Paint the faces
-        for (int i = 0; i < 8; i++)
-            if (    this. whiteFace[i] != WHITE  || this.   redFace[i] != RED    || this.greenFace[i] != GREEN ||
-                    this.yellowFace[i] != YELLOW || this.orangeFace[i] != ORANGE || this. blueFace[i] != BLUE)
-                return false;
-        return true;
+        // Setup the rotations
+        this.setupRotations();
     }
 
     // Method for imageIO
     void saveImage() {
         ImageProcessing.saveCubeImage(this);
-    }
-
-    abstract class Rotation {
-
     }
 }
